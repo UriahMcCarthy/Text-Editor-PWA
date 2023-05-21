caa57f5910bfae909016efb7d69148db9083de82
Yuri <uriahmccarthy@gmail.com>
2023-05-21 11:25:22 -0700

    de-bug code 1

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -66,8 +66,9 @@ module.exports = () => {
             options: {
               presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-
-        
+            }
+          }
+        } 
       ],
     },
   };
